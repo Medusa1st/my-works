752af0ecf0f0c0f73f86de714d4662a00de11731
Medusa1st <littlemeidusha@163.com>
2017-06-27 21:23:17 +0800

    new interface

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="cp-works">
     <div class="work-list">
-      <div class="item" v-bind:id="item.id" v-for="item of items">
+      <div class="item" v-bind:id="item.id" @click="showWorkPage(item)" v-for="item of items">
         <img src="../assets/work1.png">
         <div class="item-detail">
           <p>{{item.name}}</p>
@@ -19,16 +19,16 @@ export default {
     return {
       title: '我的作品',
       items: [
-        {name: '作品1', id: 'work-item1'},
-        {name: '作品2', id: 'work-item2'},
-        {name: '作品3', id: 'work-item3'},
+        {name: '作品1', id: 'work-item1', url: 'http://medusa1st.me/livepark/'},
+        {name: '作品2', id: 'work-item2', url: 'http://medusa1st.me/livepark/'},
+        {name: '作品3', id: 'work-item3', url: 'http://medusa1st.me/livepark/'},
       ],
       selectedItemName: ''
     }
   },
   methods: {
-    showContent(e){
-      this.selectedItemName = e.target.innerHTML;
+    showWorkPage(item){
+      window.open(item.url);
     }
   }
 }
@@ -41,31 +41,57 @@ export default {
   width: 100%;
   height: 100%;
   display: flex;
-  background-color: #efefef;
+  background-image: url("http://medusa1st.me/images/bg-wooden-style.jpg");
+  background-size:100% 100%;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  overflow-y:scroll;
 }
 .work-list{
-  width: 80vw;
+  /*width: 80vw;*/
   margin: auto;
   display: flex;
   justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
 }
 .item{
+  margin: 10px 10px;
   height: 300px;
-  margin: 0 10px;
-  flex-grow: 1;
-  border:1px solid green;
-  padding: 10px;
+  width: 300px;
+  /*flex-grow: 1;*/
+  border:1px solid #fff;
   background-color: #fff;
+  overflow: hidden;
+  box-shadow: 1px 1px 1px rgba(0,0,0,.4);
+  transition: all .5s ease;
 }
 .item img{
-  width: 100%;
+  width: calc(100% - 20px);
   height: 70%;
+  margin-top: 10px;
+  margin-left: 10px;
+  margin-right: 10px;
 }
 .item-detail{
-  background-color: rgba(0,0,0,.2);
+  height: 180px;
+  position: relative;
+  top: 0px;
+  background-color: rgba(255,255,255,.8);
+  padding: 10px;
+  transition: top .5s ease;
 }
-.item-detail p{
-  margin-top: 30px;
-  margin-bottom: 50px;
+.item-detail p:first-child{
+  padding-top: 15px;
+  margin-bottom: 40px;
+}
+.item:hover > .item-detail{
+  top: -124px;
+}
+.item:hover{
+  border:1px solid limegreen;
+  box-shadow: 10px 10px 5px rgba(0,0,0,.4);
+  height: 320px;
+  width: 320px;
 }
 </style>
