add3e15e66967e4299c7661b9d42b030a62fa383
Medusa1st <littlemeidusha@163.com>
2017-06-29 23:21:05 +0800

    modified interface

--- src/views/index.vue
+++ src/views/index.vue
@@ -2,10 +2,12 @@
   <div class="cp-works">
     <div class="work-list">
       <div class="item" v-bind:id="item.id" @click="showWorkPage(item)" v-for="item of items">
-        <img src="../assets/work1.png">
+        <div class="item-image">
+          <img :src="item.imgSrc">
+        </div>
         <div class="item-detail">
           <p>{{item.name}}</p>
-          <p>这是一段介绍文字啦啦啦，这是一段介绍文字啦啦啦，这是一段介绍文字啦啦啦，这是一段介绍文字啦啦啦，这是一段介绍文字啦啦啦，这是一段介绍文字啦啦啦。</p>
+          <p>{{item.intro}}</p>
         </div>
       </div>
     </div>
@@ -13,22 +15,41 @@
 </template>
 
 <script>
+
 export default {
   name: 'works',
   data () {
     return {
       title: '我的作品',
       items: [
-        {name: '作品1', id: 'work-item1', url: 'http://medusa1st.me/livepark/'},
-        {name: '作品2', id: 'work-item2', url: 'http://medusa1st.me/livepark/'},
-        {name: '作品3', id: 'work-item3', url: 'http://medusa1st.me/livepark/'},
+      {
+        name: '作品1-智能停车', 
+        id: 'work-item1', 
+        imgSrc: 'http://medusa1st.me/images/works/work1.png', 
+        url: 'http://medusa1st.me/livepark/', 
+        intro: '基于高德地图js-sdk的手机web页面，实现用户定位、停车位标记、停车位选择、路线导航、支付等功能。(仅供展示)'
+      },{
+        name: '作品2', 
+        id: 'work-item2', 
+        imgSrc: 'http://medusa1st.me/images/works/work-default.png', 
+        url: '', 
+        intro: '制作中……'
+      },{
+        name: '作品3', 
+        id: 'work-item3', 
+        imgSrc: 'http://medusa1st.me/images/works/work-default.png', 
+        url: '', 
+        intro: '制作中……'
+      }
       ],
       selectedItemName: ''
     }
   },
   methods: {
     showWorkPage(item){
-      window.open(item.url);
+      if(item.url != ''){
+        window.open(item.url);
+      }
     }
   }
 }
@@ -66,13 +87,21 @@ export default {
   box-shadow: 1px 1px 1px rgba(0,0,0,.4);
   transition: all .5s ease;
 }
-.item img{
+.item-image{
   width: calc(100% - 20px);
   height: 70%;
   margin-top: 10px;
   margin-left: 10px;
   margin-right: 10px;
+  text-align: center;
 }
+.item img{
+  width: 100%;
+  height: 100%;
+}
+ #work-item2 img,#work-item3 img{
+  width:70%;
+} 
 .item-detail{
   height: 180px;
   position: relative;
@@ -89,7 +118,7 @@ export default {
   top: -124px;
 }
 .item:hover{
-  border:1px solid limegreen;
+  border:1px solid #888888;
   box-shadow: 10px 10px 5px rgba(0,0,0,.4);
   height: 320px;
   width: 320px;
